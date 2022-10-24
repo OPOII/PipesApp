fcb7bf682b5fbf20685220a283f48daee70dcebf
OPOII <41926039+OPOII@users.noreply.github.com>
2022-10-23 21:17:53 -0500

    pruebas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipeApp';
+  nombre: string = 'Fernando hErRera';
+  valor: number = 1000;
+  obj = {
+    nombre: 'Fernando',
+  };
+  mostrarNombre() {
+    console.log(this.nombre);
+    console.log(this.valor);
+  }
 }
