4b26affec0ce04476e26bcb98281b1dbb16efbba
OPOII <41926039+OPOII@users.noreply.github.com>
2023-06-27 15:47:30 -0500

    create the menu bar component

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
 //Se crea este modulo para poner todo lo que se necesita en uno solo y luego si usarlo
 //en toda la app exportandolos
 @NgModule({
   declarations: [],
-  exports: [MenuModule],
+  exports: [MenuModule, MenubarModule],
 })
 export class PrimeNgModule {}
