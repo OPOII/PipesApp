3371232295a005e792aa5d8b872d45193053d8c4
OPOII <41926039+OPOII@users.noreply.github.com>
2023-06-28 12:46:54 -0500

    i18nselectpipe

--- src/app/product/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/product/pages/uncommon-page/uncommon-page.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrls: ['./uncommon-page.component.css']
+  styleUrls: ['./uncommon-page.component.css'],
 })
 export class UncommonPageComponent {
-
+  /// i18 select
+  public name: string = 'Bryan';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+  changeClient(): void {
+    this.name = 'Melisa';
+    this.gender = 'female';
+  }
 }
