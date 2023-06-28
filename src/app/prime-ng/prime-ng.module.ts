8e21299a5d23cc7ad5889765854ca0789b5566bb
OPOII <41926039+OPOII@users.noreply.github.com>
2023-06-27 21:05:06 -0500

    upper,lower and title case pipes

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
 //Se crea este modulo para poner todo lo que se necesita en uno solo y luego si usarlo
 //en toda la app exportandolos
 @NgModule({
   declarations: [],
-  exports: [MenuModule, MenubarModule],
+  exports: [
+    MenuModule,
+    MenubarModule,
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    PanelModule,
+  ],
 })
 export class PrimeNgModule {}
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -6,18 +6,15 @@ import { ProductComponent } from './product.component';
 import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
-
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [
     ProductComponent,
     BasicsPageComponent,
     NumbersPageComponent,
-    UncommonPageComponent
+    UncommonPageComponent,
   ],
-  imports: [
-    CommonModule,
-    ProductRoutingModule
-  ]
+  imports: [CommonModule, ProductRoutingModule, PrimeNgModule],
 })
-export class ProductModule { }
+export class ProductModule {}
